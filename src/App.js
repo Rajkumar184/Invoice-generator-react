b3f4dcca3ad8b340c6cf1ed329b406058ab34c75
Rajkumar184 <rajkumar.mali@nextyn.com>
2024-03-19 11:33:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -298,7 +298,7 @@ const App = () => {
           </div> */}
         </div>
 
-        <div>
+        {/* <div>
           <div class="container-fluid">
             <div class="row gutters">
               <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
@@ -399,7 +399,7 @@ const App = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
       </div>
     </div>
